f0d3e1f18a289ff912a6d8e73b8ac5cfa5c9c6a8
miinii14 <burlyaa25565@gmail.com>
2023-04-03 17:13:12 +0200

    task1.5

--- src/CustomList.java
+++ src/CustomList.java
@@ -1,42 +1,63 @@
-public class CustomList <T>{
-    private class Node{
+import java.util.NoSuchElementException;
+
+public class CustomList<T> {
+    private class Node {
         T value;
         Node next;
-        public Node(T value, Node next){
+
+        public Node(T value, Node next) {
             this.value = value;
             this.next = next;
         }
     }
-    private Node head=null, tail=null;
 
-    public void addLast(T value){
+    private Node head = null, tail = null;
+
+    public void addLast(T value) {
         Node node = new Node(value, null);
-        if(head==null){
+        if (head == null) {
             head = node;
-        }else if(head==tail){
+        } else if (head == tail) {
             head.next = node;
-        }else {
+        } else {
             tail.next = node;
         }
         tail = node;
     }
 
-    public void addFirst(T value){
+    public void addFirst(T value) {
         Node node = new Node(value, null);
-        if(head == null){
+        if (head == null) {
             head = node;
             tail = node;
-        }
-        else{
+        } else {
             node.next = head;
             head = node;
         }
     }
 
-    public T getFirst(){
+    public T removeFirst() {
+        if (head == null) {
+            throw new NoSuchElementException();
+        } else if (head == tail) {
+            T temp = head.value;
+            head = null;
+            tail = null;
+            return temp;
+        } else {
+            T temp = head.value;
+            head = head.next;
+            return temp;
+        }
+    }
+
+
+
+    public T getFirst() {
         return head.value;
     }
-    public T getLast(){
+
+    public T getLast() {
         return tail.value;
     }
 
--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,11 @@ public class Main {
     public static void main(String[] args) {
         CustomList<String> list = new CustomList<>();
         list.addLast("abc");
+        list.addLast("cdf");
         list.addLast("def");
         System.out.println(list.getFirst());
-        list.addFirst("zxc");
+        list.removeFirst();
         System.out.println(list.getFirst());
+        System.out.println(list.getLast());
     }
 }
